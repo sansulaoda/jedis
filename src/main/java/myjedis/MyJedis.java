35a697037b55fde0a10f80d9dd4af8edf583eb61
yhdong <1056895949@qq.com>
2018-01-16 19:04:51 +0800

    upt 01

---
+++ src/main/java/myjedis/MyJedis.java
@@ -0,0 +1,22 @@
+package myjedis;
+
+import java.util.Set;
+
+import redis.clients.jedis.Jedis;
+
+
+public class MyJedis {
+	
+	public static void main(String[] args) throws InterruptedException {
+		Jedis jedis = null;
+		jedis = new Jedis("192.168.1.101",6379);
+		//jedis.set("k1", "v1");
+		//jedis.lpush("k2","l1","l2","l3");
+		//jedis.rpush("k3", "r1","r2","r3");
+		Set keys = jedis.keys("*");
+		for (Object string : keys) {
+			System.out.println(string);
+		}
+		Thread.sleep(100);
+	}
+}
